def5b2eefa68c5a42d090b79fd36ec3190ffdfa4
Werik Rodrigues <werikrodrigues1@hotmail.com.br>
2023-12-13 12:12:27 -0300

    add post functions

---
+++ post_app/lib/features/posts/repos/posts_repo.dart
@@ -0,0 +1,50 @@
+import 'dart:convert';
+import 'dart:developer';
+import 'package:http/http.dart' as http;
+import 'package:post_app/features/posts/models/post_data_ui_model.dart';
+
+class PostsRepo {
+  static Future<List<PostDataUiModel>> fetchPosts() async {
+    var client = http.Client();
+    List<PostDataUiModel> posts = [];
+    try {
+      var response = await client
+          .get(Uri.parse('https://jsonplaceholder.typicode.com/posts'));
+
+      List result = jsonDecode(response.body);
+
+      for (int i = 0; i < result.length; i++) {
+        PostDataUiModel post =
+            PostDataUiModel.fromMap(result[i] as Map<String, dynamic>);
+        posts.add(post);
+      }
+
+      return posts;
+    } catch (e) {
+      log(e.toString());
+      return [];
+    }
+  }
+
+  static Future<bool> addPost() async {
+    var client = http.Client();
+
+    try {
+      var response = await client
+          .post(Uri.parse('https://jsonplaceholder.typicode.com/posts'), body: {
+        "title": "Werik Aprendendo Flutter",
+        "body": "Werik falando sobre bloc",
+        "userId": "40",
+      });
+
+      if (response.statusCode >= 200 && response.statusCode < 300) {
+        return true;
+      } else {
+        return false;
+      }
+    } catch (e) {
+      log(e.toString());
+      return false;
+    }
+  }
+}
